dbf584cabf6ad6f4d40f5da2d1e54905e3f1b8d6
KamakuraYaru <51132080+KamakuraYaru@users.noreply.github.com>
2020-09-11 07:32:30 +0200

    Initial Commit

---
+++ Pflanzenbestimmung-Desktop/Benutzerverwaltung.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pflanzenbestimmung_Desktop
+{
+    /// <summary>
+    /// Interaktionslogik für Benutzerverwaltung.xaml
+    /// </summary>
+    public partial class Benutzerverwaltung : UserControl
+    {
+        public Benutzerverwaltung()
+        {
+            InitializeComponent();
+        }
+    }
+}
