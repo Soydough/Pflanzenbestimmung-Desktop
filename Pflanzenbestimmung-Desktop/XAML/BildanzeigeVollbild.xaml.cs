fbb1f520961aaa06aaca4b60af41e8cd3ad0332d
KamakuraYaru <51132080+KamakuraYaru@users.noreply.github.com>
2020-10-26 15:37:21 +0100

    Abdeit

--- Pflanzenbestimmung-Desktop/XAML/BildanzeigeVollbild.xaml.cs
+++ Pflanzenbestimmung-Desktop/XAML/BildanzeigeVollbild.xaml.cs
@@ -15,17 +15,9 @@ namespace Pflanzenbestimmung_Desktop.XAML
             InitializeComponent();
 
             Bild.Source = Main.fullscreenImage;
-            PreviewKeyDown += new KeyEventHandler(HandleEsc);
 
             Bild.MouseUp += ImageClickEvent;
-
             KeyDown += KeyPressEvent;
-        }
-
-        private void HandleEsc(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-                Close();
         }
 
         void ImageClickEvent(object sender, EventArgs e)
@@ -33,11 +25,14 @@ namespace Pflanzenbestimmung_Desktop.XAML
             Close();
         }
 
+        //Dieser Code gehört so. Bitte nicht ändern. Danke.
         Key[] \u006B\u006F\u005F\u006E\u0061\u005F\u005F\u006D\u0069 = new Key[] { \u004B\u0065\u0079.\u0055\u0070, \u004B\u0065\u0079.\u0055\u0070, \u004B\u0065\u0079.\u0044\u006F\u0077\u006E, \u004B\u0065\u0079.\u0044\u006F\u0077\u006E, \u004B\u0065\u0079.\u004C\u0065\u0066\u0074, \u004B\u0065\u0079.\u0052\u0069\u0067\u0068\u0074, \u004B\u0065\u0079.\u004C\u0065\u0066\u0074, \u004B\u0065\u0079.\u0052\u0069\u0067\u0068\u0074, \u004B\u0065\u0079.\u0042, \u004B\u0065\u0079.\u0041 };
         int index = 0;
         void KeyPressEvent(object sender, KeyEventArgs e)
         {
-            if (new Key[]{ \u004B\u0065\u0079.\u0055\u0070, \u004B\u0065\u0079.\u0044\u006F\u0077\u006E, \u004B\u0065\u0079.\u004C\u0065\u0066\u0074, \u004B\u0065\u0079.\u0052\u0069\u0067\u0068\u0074, \u004B\u0065\u0079.\u0041, \u004B\u0065\u0079.\u0042 }.Contains(e.Key))
+            if (e.Key == \u004B\u0065\u0079.\u0045\u0073\u0063\u0061\u0070\u0065)
+                \u0043\u006C\u006F\u0073\u0065();
+            else if (new Key[]{ \u004B\u0065\u0079.\u0055\u0070, \u004B\u0065\u0079.\u0044\u006F\u0077\u006E, \u004B\u0065\u0079.\u004C\u0065\u0066\u0074, \u004B\u0065\u0079.\u0052\u0069\u0067\u0068\u0074, \u004B\u0065\u0079.\u0041, \u004B\u0065\u0079.\u0042 }.Contains(e.Key))
             {
                 if(\u006B\u006F\u005F\u006E\u0061\u005F\u005F\u006D\u0069[index] == e.Key)
                 {
