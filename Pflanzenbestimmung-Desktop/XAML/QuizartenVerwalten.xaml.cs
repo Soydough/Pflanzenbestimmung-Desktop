7c4c2ac2e6ad8fa3d180317c6d01ab8fe898e079
Jafa95 <61741048+Jafa95@users.noreply.github.com>
2020-10-28 11:02:44 +0100

    Hotfix Quizartenfenster

--- Pflanzenbestimmung-Desktop/XAML/QuizartenVerwalten.xaml.cs
+++ Pflanzenbestimmung-Desktop/XAML/QuizartenVerwalten.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Pflanzenbestimmung_Desktop
+namespace Pflanzenbestimmung_Desktop.XAML
 {
     /// <summary>
     /// Interaktionslogik für QuizartenVerwalten.xaml
